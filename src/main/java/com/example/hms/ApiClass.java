a489399fcc7a55f98f1654560a92dfc9d1a50175
RajkumarRavadi <rajk142567@gmail.com>
2024-07-18 10:22:46 +0530

    Added code for get and post patient info

--- src/main/java/com/example/hms/ApiClass.java
+++ src/main/java/com/example/hms/ApiClass.java
@@ -1,11 +1,14 @@
 package com.example.hms;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ApiClass {
+
+    //creating a hashmap to store the data
+    HashMap<Integer, Patient> patientDB = new HashMap<>();
+
     @GetMapping("getWeatherInfo")
     public String getWeatherInfo(){
         return "The temperature is 35.";
@@ -16,5 +19,21 @@ public class ApiClass {
                           @RequestParam("num2") Integer no2){
         Integer total = no1 + no2;
         return total;
+    }
+
+    @PostMapping("addPatient")
+    public String  addPatient(@RequestBody Patient patient){
+        int key = patient.getPatientId();
+        //ADD THIS TO PATIENT DB
+        patientDB.put(key, patient);
+
+        return "The patient with key "+key+"has added to the BD";
+    }
+
+    @GetMapping("getPatientInfo")
+    public Patient getPatientInfo(@RequestParam("patientId") Integer patientId){
+        Patient patient;
+        patient = patientDB.get(patientId);
+        return patient;
     }
 }
---
+++ src/main/java/com/example/hms/Patient.java
@@ -0,0 +1,30 @@
+package com.example.hms;
+
+public class Patient {
+    private int patientId;
+    private String patientName;
+    private int patientAge;
+    private String disease;
+    private String mobNum;
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public int getPatientAge() {
+        return patientAge;
+    }
+
+    public String getDisease() {
+        return disease;
+    }
+
+    public String getMobNum() {
+        return mobNum;
+    }
+
+}
